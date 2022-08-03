7a6e44a876c646367be1dc9cd298d7c6ec6c2431
Maximilian Staab <maximilian.staab.de@gmail.com>
2022-08-03 18:55:31 +0200

    Add parameter to specify the name of the database

--- muell/config.py
+++ muell/config.py
@@ -6,6 +6,7 @@ def config():
     return {
         "host": os.getenv("POSTGRESQL_HOST"),
         "user": os.getenv("POSTGRESQL_USER", "postgres"),
+        "database": os.getenv("POSTGRESQL_DB", "postgres"),
         "password": os.getenv("POSTGRESQL_SECRET"),
         "port": os.getenv("POSTGRESQL_PORT", 5432)
     }
